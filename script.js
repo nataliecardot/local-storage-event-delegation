e7cbcaf4eef015010c5c012d35799a00cc315b4b
Natalie-Cardot <nataliecardot@gmail.com>
2021-04-15 18:28:20 -0700

    Persist checkbox state in session storage

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const addItems = document.querySelector('.add-items');
-const itemsList = document.querySelector('.plates');
+const addItems = document.querySelector('.add-items'); // form
+const itemsList = document.querySelector('.plates'); // ul
 const items = JSON.parse(sessionStorage.getItem('items')) || [];
 
 function addItem(e) {
@@ -33,6 +33,16 @@ function populateList(plates = [], platesList) {
     .join('');
 }
 
+function toggleDone(e) {
+  if (!e.target.matches('input')) return;
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  sessionStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
 addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
 
 populateList(items, itemsList);
